b3cd6219ba8d47c432a956417726403987371347
Igor Zmitrachkov <zmitrachkov_igor@mail.ru>
2021-12-29 15:30:15 +0500

    class browser

---
+++ demo_card/include/browser.h
@@ -0,0 +1,33 @@
+#define bufsize      6
+
+class browser {
+ private:
+  String buf[bufsize]; // размер буфера вывода в строках
+  String* curfile; // указатель на строку с названием текущего файла
+  struct fileContext {
+    /*
+      Контекст файла. Содержит открытый коренной каталог и сам файл,
+      а также методы для вывода и закрытия каталога и файла
+    */
+    File root;
+    File curfile;
+    void print() const;
+    void close();
+  };
+ public:
+  browser();
+  ~browser();
+  void print();
+  File getFirstFile();
+  void fillFirstBuf();
+  void clearBuf();
+  void moveBufUp();
+  void moveBufDown();
+  void moveCurfileUp();
+  void moveCurfileDown();
+  char curfilePosInBuf();
+  File prevFile();
+  File nextFile();
+  fileContext curfileContext();
+  String* getCurfile();
+};
